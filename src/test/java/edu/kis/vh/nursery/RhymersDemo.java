7fdcaf0f3fa26e24b3f3ccdfe5cf9370d2e42835
ShouldIStayOrShouldIGo <natbart847@gmail.com>
2020-01-06 15:16:32 +0100

    RhymersDemo method

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -8,8 +8,7 @@ class RhymersDemo {
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
 
-        DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+        DefaultCountingOutRhymer[] rhymers = testRhymers(factory);
 
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
@@ -28,6 +27,11 @@ class RhymersDemo {
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
 
+    }
+
+    private static DefaultCountingOutRhymer[] testRhymers(Rhymersfactory factory) {
+        return new DefaultCountingOutRhymer[]{factory.getStandardRhymer(), factory.getFalseRhymer(),
+                    factory.getFIFORhymer(), factory.getHanoiRhymer()};
     }
 
 }
