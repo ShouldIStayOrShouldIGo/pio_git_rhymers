e347c3954620493caf9c42de18b17f5575df6e65
ShouldIStayOrShouldIGo <natbart847@gmail.com>
2020-01-06 18:21:49 +0100

    Diagram & comments

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -6,21 +6,24 @@ import edu.kis.vh.nursery.HanoiRhymer;
 
 public class DefaultRhymersFactory implements Rhymersfactory {
 
+    // Klasa ta wytwarza instancje klas, które dzidziczą po DefaultCountingOutRhymer
+
+    //return zwraca instancje klasy DefaultCountingOutRhymer
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
-
+    //return zwraca instancje klasy DefaultCountingOutRhymer
     @Override
     public DefaultCountingOutRhymer getFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
-
+    //return zwraca instancje klasy FIFORhymer
     @Override
     public DefaultCountingOutRhymer getFIFORhymer() {
         return new FIFORhymer();
     }
-
+    //return zwraca instancje klasy HanoiRhymer
     @Override
     public DefaultCountingOutRhymer getHanoiRhymer() {
         return new HanoiRhymer();
